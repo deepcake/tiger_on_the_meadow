51a9ff66cd7b24571fa195bf269d787836446955
deepcake <apurefocus@gmail.com>
2019-09-28 20:40:28 +0300

    fixed out of bounds

--- src/systems/Movement.hx
+++ src/systems/Movement.hx
@@ -19,10 +19,12 @@ class Movement extends System {
 
         if (pos.x + dx >= w || pos.x + dx <= 0) {
             vel.x *= -1;
+            dx *= -1;
         }
 
         if (pos.y + dy >= h || pos.y + dy <= 0) {
             vel.y *= -1;
+            dy *= -1;
         }
 
         pos.x += dx;
